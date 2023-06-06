9a41a4bfa577f46a2719f06fd724d2a854263c41
Ben-21 <bs@schaefer-inet.de>
2023-06-06 11:20:16 +0200

    initial commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+
+    //Fields
+    private String name;
+    private String id;
+
+
+
+    //Methods
+    public void addStudent(String name, String id){
+
+    }
+    public void removeStudent(String name, String id){
+
+    }
+
+
+
+
+
+}
